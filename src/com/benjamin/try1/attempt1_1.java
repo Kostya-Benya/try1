5d0982bb490d6a330b8d95d4fedc9ad6eab918e5
Kostya-Benya Boguslavsky <drova311@gmail.com>
2019-07-09 16:03:26 +0300

    second attemption

--- src/com/benjamin/try1/attempt1_1.java
+++ src/com/benjamin/try1/attempt1_1.java
@@ -2,6 +2,12 @@ package com.benjamin.try1;
 
 public class attempt1_1 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!!! It's me!!!");
+        
+        String s1 = "Vasya";
+        String s2 = "Vasya";
+        String s3 = "Petya";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s3));
     }
-    }
+}
